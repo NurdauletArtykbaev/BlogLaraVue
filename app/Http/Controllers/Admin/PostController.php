1f547f813d590b3b3d117f899c6f500aed4d9d48
unknown <artykbaev.nurdaulet@mail.ru>
2020-06-08 23:05:52 +0600

    Database Relationship One to Many part - 7

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function all_posts(){
+        // $posts = Post::with('user')->get();
+        // $posts = Post::with('category')->get();
+        // $posts = Post::all();
+        $category = Category::with('posts')->get();
+        return $category;
+
+        // return $posts;
+    }
 }
