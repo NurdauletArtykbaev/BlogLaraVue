3cc2157840af69105d8f4a32308abef6d0b8bb95
nartykbaev <artykbaev.nurdaulet@mail.ru>
2020-06-09 09:09:23 +0600

    Install Vue Router part - 8

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function all_posts(){
+//        $posts = Post::all();
+//        $category = Post::with('category')->get();
+        $user = Post::with('user')->get();
+        return $user;
+    }
 }
---
+++ resources/js/routes.js
@@ -0,0 +1,15 @@
+import VueRouter from 'vue-router'
+
+let routes = [
+    {
+
+    },
+
+]
+
+let router = new VueRouter({
+    routes,
+    mode:'history'
+});
+
+export default router
