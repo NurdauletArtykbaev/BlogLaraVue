f750a4caf5fb8a6c7773afa8915aeb4d4c715350
nartykbaev <artykbaev.nurdaulet@mail.ru>
2020-06-12 19:02:26 +0600

    How to setup vuex in laravel project part - 17

---
+++ resources/js/components/store/index.js
@@ -0,0 +1,18 @@
+//https://vuex.vuejs.org/ru/guide/
+export default {
+    state:{
+        category:[],
+    },
+    getters:{
+        getCategory(state){
+
+        }
+
+    },
+    actions:{
+
+    },
+    mutations:{
+
+    },
+}
